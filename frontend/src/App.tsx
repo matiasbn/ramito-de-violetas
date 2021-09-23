895ba60cbbe0634aec58289066ad569af6bbf25c
Matías Barrios <matias.barriosn@gmail.com>
2021-09-23 19:16:04 -0300

    feat: generating random number off chain

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,38 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import { NavBar } from './components/NavBar';
 import { AppProvider } from './store';
-import { Home } from './views/Home';
+import { Button, Card, CardGroup, Col, Container } from 'react-bootstrap';
 
 function App() {
+  const [number, setNumber] = useState((Math.random() * 1000) | 0);
+  function handleRandomNumber() {
+    setNumber((Math.random() * 1000) | 0);
+  }
   return (
     <AppProvider>
       <div className="App">
         <NavBar />
-        <Home />
+        <Container>
+          <Col>
+            <CardGroup>
+              <Card>
+                <Card.Img variant="top" src="holder.js/100px160" />
+                <Card.Body>
+                  <Card.Title>Ramito de violetas</Card.Title>
+                  <Card.Text>Random number:{number}</Card.Text>
+                  <Button onClick={handleRandomNumber} variant="primary">
+                    Generate Ramito
+                  </Button>
+                </Card.Body>
+                <Card.Footer>
+                  <small className="text-muted">Last updated 3 mins ago</small>
+                </Card.Footer>
+              </Card>
+            </CardGroup>
+          </Col>
+        </Container>
       </div>
     </AppProvider>
   );
